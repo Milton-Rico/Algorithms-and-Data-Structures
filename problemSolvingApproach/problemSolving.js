775b5848af1835f8bfd5a1ee27ac9380596ec8f0
Milton Rico <miltonrico02@gmail.com>
2025-08-06 15:05:59 -0500

    refactor charCount function to use 'result' instead of 'obj' and add isAlfanumeric helper function

--- problemSolvingApproach/problemSolving.js
+++ problemSolvingApproach/problemSolving.js
@@ -28,18 +28,18 @@ console.log(sumOf2Numbers(5, 8));
 // 4. Explore Examples with Invalid Inputs
 
 function charCount(str) {
-  var obj = {};
+  var result = {};
   for (var i = 0; i < str.length; i++) {
     var char = str[i].toLowerCase();
-    if (obj[char]) {
-      obj[char]++;
+    if (result[char]) {
+      result[char]++;
     } else {
-      obj[char] = 1;
+      result[char] = 1;
     }
   }
-  return console.log(obj);
+  return console.log(result);
 }
-charCount('hello this es 1 2 3');
+// charCount('hello this es 1 2 3');
 
 //BREAK IT DOWN
 // Explicitly write out the steps you need to take.
@@ -52,6 +52,7 @@ function charCount(str) {
   // loop through string, for each char
   // if char is a number/letter, and exists in obj, add 1
   // if char is a number/letter, and doesn't exist in obj, add it to obj with value of 1
+  // if the char is a space, do nothing
   // return obj
 }
 
@@ -61,3 +62,86 @@ function charCount(str) {
 //2.  Temporarily ignore that difficulty
 //3.  Write a simplified solution
 //4.  Then incorporate that difficulty back in
+
+// REFACTORING QUESTIONS
+// 1. Can you check the result?
+// 2. Can you derive the result differently?
+// 3. Can you understand it at a glance?
+// 4. Can you use the result or method for some other problem?
+// 5. Can you improve the performance of your solution?
+// 6. Can you think of other ways to refactor?
+// 7. How have other people solved this problem?
+
+// One solution to refactor
+// function charCount2(str) {
+//   var result = {};
+//   for (var i = 0; i < str.length; i++) {
+//     var char = str[i].toLowerCase();
+//     if (/a-z0-9/.test(char)) {
+//       if (result[char]) {
+//         result[char]++;
+//       } else {
+//         result[char] = 1;
+//       }
+//     }
+//     return console.log(result);
+//   }
+// }
+// charCount2('hello this es 1 2 3');
+
+// Another solution to refactor
+// function charCount2(str) {
+//   var result = {};
+//   for (var char of str) {
+//     char = char.toLowerCase();
+//     if (/[a-z0-9]/.test(char)) {
+//       if (result[char] > 0) {
+//         result[char]++;
+//       } else {
+//         result[char] = 1;
+//       }
+//     }
+//   }
+//   console.log(result);
+//   return result;
+// }
+// charCount2('hello this es 1 2 3');
+
+// function charCount2(str) {
+//   var result = {};
+//   for (var char of str) {
+//     char = char.toLowerCase();
+//     if (/[a-z0-9]/.test(char)) {
+//       result[char] = result[char] + 1 || 1;
+//     }
+//   }
+//   console.log(result);
+//   return result;
+// }
+// charCount2('hello this es 1 2 3');
+
+function charCount2(str) {
+  var result = {};
+  for (var char of str) {
+    char = char.toLowerCase();
+    if (isAlfanumeric(char)) {
+      result[char] = result[char] + 1 || 1;
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+function isAlfanumeric(char) {
+  var code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) && // numeric (0-9)
+    !(code > 64 && code < 91) && // upper alpha (A-Z)
+    !(code > 96 && code < 123) // lower alpha (a-z)
+  ) {
+    return false;
+  }
+  return true;
+}
+
+charCount2('hello this es 1 2 3');
