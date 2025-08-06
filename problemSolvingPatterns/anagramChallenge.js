df43f305ed3e10b9cb9e316c6cd8f791e8f2d019
Milton Rico <miltonrico02@gmail.com>
2025-08-06 17:11:14 -0500

    updating anagram solution

--- problemSolvingPatterns/anagramChallenge.js
+++ problemSolvingPatterns/anagramChallenge.js
@@ -6,22 +6,41 @@ function same1(str1, str2) {
   str1 = str1.split('').sort().join('');
   str2 = str2.split('').sort().join('');
 
-  let frequencyCounter1 = {};
-  let frequencyCounter2 = {};
-
-  for (let val of str1) {
-    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  if (str1 !== str2) {
+    return false;
   }
-  for (let val of str2) {
-    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
-  }
-
-  console.log(frequencyCounter1, frequencyCounter2);
-  for (let key in frequencyCounter1) {
-    if (frequencyCounter2[key] !== frequencyCounter1[key]) {
-      return false;
-    }
-  }
+  console.log(str1, str2);
   return true;
 }
-console.log(same1('azaz', 'zaaz'));
+console.log(same1('rat', 'car'));
+
+// Teachers solution
+// function validAnagram(first, second) {
+//   if (first.length !== second.length) {
+//     return false;
+//   }
+
+//   const lookup = {};
+
+//   for (let i = 0; i < first.length; i++) {
+//     let letter = first[i];
+//     // if letter exists, increment, otherwise set to 1
+//     lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+//   }
+//   console.log(lookup)
+
+//   for (let i = 0; i < second.length; i++) {
+//     let letter = second[i];
+//     // can't find letter or letter is zero then it's not an anagram
+//     if (!lookup[letter]) {
+//       return false;
+//     } else {
+//       lookup[letter] -= 1;
+//     }
+//   }
+
+//   return true;
+// }
+
+// // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
+// validAnagram('anagrams', 'nagaramm')
