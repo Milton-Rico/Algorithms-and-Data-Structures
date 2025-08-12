d5bc339d648b51978ba26b98fe176b85bc800bb7
Milton Rico <miltonrico02@gmail.com>
2025-08-12 11:52:04 -0500

    maxSubarraySum completed

--- problemSolvingPatterns/optionalChallenges/maxSubarraySum.js
+++ problemSolvingPatterns/optionalChallenges/maxSubarraySum.js
@@ -4,4 +4,25 @@
 // maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2) // 5
 // maxSubarraySum([2,3], 3) // null
 
-function maxSubarraySum() {}
+function maxSubarraySum(arr, num) {
+  if (arr.length < num) return null;
+
+  let max = 0;
+  let subTotal = 0;
+
+  for (let i = 0; i < num; i++) {
+    max += arr[i];
+  }
+  subTotal = max;
+  for (let i = num; i < arr.length; i++) {
+    subTotal = subTotal + arr[i] - arr[i - num];
+    max = Math.max(max, subTotal);
+  }
+  return max;
+}
+
+console.log(maxSubarraySum([100, 200, 300, 400], 2));
+console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4));
+console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2));
+console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2));
+console.log(maxSubarraySum([2, 3], 3));
